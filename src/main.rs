1c0a7e0018aa7da912877f220199d836d2990764
nicolas <joulin.nicolas@gmail.com>
2020-07-26 16:33:46 +0200

    cli options taken in, rotate next

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,26 @@ fn main() {
     let matches = cli::en();
     let image_input_path = matches.value_of("INPUT").unwrap();
     let image_output_path = format!("{}{}", utils::generate_id(), ".png");
+    println!("l? {}, u? {}", matches.is_present("LOWER_THRESHOLD"), matches.is_present("UPPER_THRESHOLD"));
     println!("opening image {}", image_input_path);
-    let img = image::open(image_input_path).unwrap().into_rgb();
+    let img = image::open(image_input_path).unwrap().rotate270().to_rgb();
 
-    match matches.subcommand_name() {
-      Some("threshold") => {
-       println!("using threshold");
-       let intervals = intervals::threshold(&img, 32, 223);
-       let output_img = sorter::sort(img, intervals);
-       output_img.save(image_output_path).expect("something went wrong when saving file.");
+  if let Some(matches) = matches.subcommand_matches("threshold") {
+    println!("using threshold");
+    let lower = if matches.is_present("lower") {
+      matches.value_of("lower").unwrap().parse().expect("lower threshold has to be an integer between 0 and 255")
+    } else {
+      32
+    };
+    let upper = if matches.is_present("upper") {
+      matches.value_of("upper").unwrap().parse().expect("upper threshold has to be an integer between 0 and 255")
+    } else {
+      223
+    };
+    println!("lower threshold : {}, upper threshold : {}", lower, upper);
+    let intervals = intervals::threshold(&img, lower, upper);
+    let output_img = image::DynamicImage::ImageRgb8(sorter::sort(img, intervals));
 
-      },
-      _ => {},
-    }
+    output_img.rotate90().save(image_output_path).expect("something went wrong when saving file.");
+  }
 }
--- src/sorter.rs
+++ src/sorter.rs
@@ -1,14 +1,14 @@
-use image::{Rgb, RgbImage, ImageBuffer, Pixel};
+use image::RgbImage;
 use std::ops::Range;
 use crate::utils::lightness;
 
-pub fn sort(img: RgbImage, intervals: Vec<Range<usize>>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+pub fn sort(img: RgbImage, intervals: Vec<Range<usize>>) -> RgbImage {
   let mut pixels: Vec<_> = img.pixels().collect();
 
   for interval in intervals.into_iter() {
     pixels[interval].sort_by(|a,b| lightness(a).cmp(&lightness(b)));
   }
-  let mut buf = ImageBuffer::new(img.width(), img.height());
+  let mut buf = RgbImage::new(img.width(), img.height());
 
   for (i, buf_p) in buf.pixels_mut().enumerate() {
     *buf_p = *pixels[i];
